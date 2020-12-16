d996be522092cb1bc90bf858a2202bf51d791bc5
Steve-Hsu <wuujdsc@gmail.com>
2020-12-16 16:34:09 +0800

    fix port

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
